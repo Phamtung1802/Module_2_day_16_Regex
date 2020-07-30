214ad302e4a50a0613696370e2fbe01675f43c52
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-30 14:06:57 +0700

    update ten lop

---
+++ BaiTapTenLopHoc/src/TenLopHoc.java
@@ -0,0 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TenLopHoc {
+        private static Pattern pattern;
+        private Matcher matcher;
+
+        private static final String EMAIL_REGEX =   "^[CAP][0-9]{4}[GHIKLM]$";
+
+        public TenLopHoc() {
+            pattern = Pattern.compile(EMAIL_REGEX);
+        }
+
+        public boolean validate(String regex) {
+            return pattern.matcher(regex).matches();
+        }
+
+    public static void main(String[] args) {
+      TenLopHoc tenLopHoc;
+      final String[] validEmail = new String[] { "C0318G"};
+      final String[] invalidEmail = new String[] { "M0318G", "0323A" };
+
+        tenLopHoc = new TenLopHoc();
+        for (String email : validEmail) {
+            boolean isvalid = tenLopHoc.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+        for (String email : invalidEmail) {
+            boolean isvalid = tenLopHoc.validate(email);
+            System.out.println("Email is " + email +" is valid: "+ isvalid);
+        }
+
+    }
+}
+
+
